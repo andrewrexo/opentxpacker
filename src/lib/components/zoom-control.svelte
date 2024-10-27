7ad8cc37870e38e21ed3476a600ff3f13e345d42
Andrew R <beta@runelite.bot>
2024-10-27 10:56:03 -0500

    add separate zoom control component

---
+++ src/lib/components/zoom-control.svelte
@@ -0,0 +1,18 @@
+<script>
+	let zoom = $state(100);
+
+	const zoomIn = () => {
+		zoom = zoom + 10;
+	};
+
+	const zoomOut = () => {
+		zoom = zoom - 10;
+	};
+</script>
+
+<div class="z-10 flex items-center gap-2">
+	<button class="btn btn-circle btn-sm" onclick={zoomOut}>-</button>
+	<input type="range" class="range range-sm w-32" min="10" max="200" bind:value={zoom} />
+	<button class="btn btn-circle btn-sm" onclick={zoomIn}>+</button>
+	<span class="text-sm">{zoom}%</span>
+</div>
