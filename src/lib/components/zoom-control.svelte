616c660435044d686f15e02467c68abfbcf0286c
Andrew R <beta@runelite.bot>
2024-10-29 13:30:27 -0500

    tie zoom values to phaser camera

--- src/lib/components/zoom-control.svelte
+++ src/lib/components/zoom-control.svelte
@@ -1,18 +1,38 @@
-<script>
-	let zoom = $state(100);
+<script lang="ts">
+	import eventBus from '$lib/phaser/event-bus';
+
+	let zoom = $state(50);
 
 	const zoomIn = () => {
-		zoom = zoom + 10;
+		if (zoom + 25 > 200) return;
+		zoom = zoom + 25;
+		eventBus.emit('adjustZoom', zoom);
 	};
 
 	const zoomOut = () => {
-		zoom = zoom - 10;
+		if (zoom - 25 < 25) return;
+		zoom = zoom - 25;
+		eventBus.emit('adjustZoom', zoom);
 	};
+
+	eventBus.on('adjustZoom', (value: number) => {
+		zoom = value;
+	});
 </script>
 
-<div class="z-10 flex items-center gap-2">
+<div class="z-10 flex items-center gap-2 rounded-full bg-base-200 p-2">
 	<button class="btn btn-circle btn-sm" onclick={zoomOut}>-</button>
-	<input type="range" class="range range-sm w-32" min="10" max="200" bind:value={zoom} />
+	<input
+		type="range"
+		class="range range-sm w-32"
+		min="25"
+		max="200"
+		step="25"
+		bind:value={zoom}
+		onchange={() => {
+			eventBus.emit('adjustZoom', zoom);
+		}}
+	/>
 	<button class="btn btn-circle btn-sm" onclick={zoomIn}>+</button>
 	<span class="text-sm">{zoom}%</span>
 </div>
